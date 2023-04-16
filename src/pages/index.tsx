05c6d1cda339ef35d95d82ac873633208880e389
Richard Nemeth <nemethricsi@Richards-Private-MacBook-Pro.local>
2023-04-16 12:58:18 +0200

    add social icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,19 @@ import styled from 'styled-components';
 import { StaticImage } from "gatsby-plugin-image"
 import Seo from '../components/Seo';
 import { toast } from 'react-toastify';
+import { SiBandcamp,  SiYoutube } from 'react-icons/si'
+import { SlSocialFacebook, SlSocialInstagram, SlSocialSpotify, SlSocialSoundcloud } from 'react-icons/sl'
 
 const MainStyles = styled.main`
   min-height: 100%;
   display: grid;
   place-content: center;
+`;
+
+const ImageWrapper = styled.div`
+  justify-self: center;
+  margin-bottom: 1rem;
+  width: 200px;
 `;
 
 const FormWrapper = styled.div`
@@ -83,12 +91,6 @@ const FormWrapper = styled.div`
     padding: 2.5rem;
   }
 `;
-  
-const ImageWrapper = styled.div`
-  justify-self: center;
-  margin-bottom: 2rem;
-  width: 200px;
-`;
 
 const FormTextWrapper = styled.div`
   display: flex;
@@ -131,6 +133,40 @@ const SubscriptionImageWrapper = styled.div`
   }
 `;
 
+const SocialIcons = styled.div`
+  padding: 20px; 
+  margin-bottom: 5rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1.5rem;
+
+  @media (max-width: 800px) {
+    margin-bottom: 2rem;
+  }
+`;
+
+const IconWrapper = styled.a`
+  background: white;
+  width: 50px;  
+  height: 50px; 
+  border-radius: 50%;
+  display: grid; 
+  place-content: center;
+  cursor: pointer;
+  box-shadow: var(--icon-wrapper-box-shadow);
+  transition: transform 0.25s ease-in-out;
+
+  &:hover {
+    box-shadow: var(--box-shadow-paper);
+  }
+
+  @media (max-width: 800px) {
+    width: 60px;  
+    height: 60px;
+  }
+`;
+
 const IndexPage: React.FC<PageProps> = () => {
   const [loading, setLoading] = React.useState(false);
 
@@ -168,7 +204,50 @@ const IndexPage: React.FC<PageProps> = () => {
         <ImageWrapper>
           <StaticImage src="../assets/images/ftm-logo.jpeg" alt="Feed The Mogul logo" style={{ borderRadius: '50%', width: '100%' }} />
         </ImageWrapper>
-        {/* <div style={{ padding: 20, border: '1px solid deeppink', marginBottom: '2.5rem' }}>SOCIAL ICONS</div> */}
+        <SocialIcons>
+          <IconWrapper 
+            href="https://feedthemogul.bandcamp.com"
+            target="_blank"
+            rel='noopener noreferrer'
+            >
+            <SiBandcamp size={30} />
+          </IconWrapper>
+          <IconWrapper 
+            href="https://open.spotify.com/artist/0sIHN7AhJVinHJak74LClq?si=DzI9EyYQRpGEts1SuwugXg&dd=1"
+            target="_blank"
+            rel='noopener noreferrer'
+            >
+            <SlSocialSpotify size={30} />
+          </IconWrapper>
+          <IconWrapper 
+            href="https://www.instagram.com/feedthemogul"
+            target="_blank"
+            rel='noopener noreferrer'
+            >
+            <SlSocialInstagram size={30} />
+          </IconWrapper>
+          <IconWrapper 
+            href="https://www.facebook.com/feedthemogul"
+            target="_blank"
+            rel='noopener noreferrer'
+            >
+            <SlSocialFacebook size={30} />
+          </IconWrapper>
+          <IconWrapper 
+            href="https://www.youtube.com/@FeedTheMogul"
+            target="_blank"
+            rel='noopener noreferrer'
+            >
+            <SiYoutube size={30} />
+          </IconWrapper>
+          <IconWrapper 
+            href="https://soundcloud.com/user-512421796"
+            target="_blank"
+            rel='noopener noreferrer'
+            >
+            <SlSocialSoundcloud size={30} />
+          </IconWrapper>
+        </SocialIcons>
         <FormWrapper >
           <FormTextWrapper>
             <SubscriptionImageWrapper>
